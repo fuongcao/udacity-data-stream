c3c1f28bb439cca092c19ad2cdf249472cab1090
fuongcao <caohoaiphuong90@gmail.com>
2024-12-13 00:03:41 +0700

    producer clean ...

--- data-ingestion-with-kafka/producers/connector.py
+++ data-ingestion-with-kafka/producers/connector.py
@@ -39,7 +39,7 @@ def configure_connector():
                 "value.converter.schemas.enable": "false",
                 "batch.max.rows": "500",
                 # TODO
-                "connection.url": "jdbc:postgresql://localhost:5433/cta",
+                "connection.url": "jdbc:postgresql://postgres:5432/cta",
                 # TODO
                 "connection.user": "cta_admin",
                 # TODO
@@ -50,7 +50,7 @@ def configure_connector():
                 "mode": "incrementing",
                 # TODO
                 "incrementing.column.name": "stop_id",
-                "topic.prefix": "org.chicago.cta.connectors.",
+                "topic.prefix": "org.chicago.cta.connector.",
                 # TODO
                 "poll.interval.ms": "5000"
             }
